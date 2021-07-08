d942e10d82a81a2f4437898030501dfa96646b95
Dawid Gacek <d.gacek@wasko.pl>
2021-07-08 09:37:19 +0200

    added user entity

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    private String email;
+    private Long phone;
+    private String username;
+    private String password;
+}
