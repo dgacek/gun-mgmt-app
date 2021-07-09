bfa7f2cb27bab374cae82cc94aaf0049fa835df6
Dawid Gacek <d.gacek@wasko.pl>
2021-07-09 12:28:40 +0200

    changed GenerationType to IDENTITY in all entities

--- src/main/java/com/example/demo/entity/TypeEntity.java
+++ src/main/java/com/example/demo/entity/TypeEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class TypeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Table(name = "users")
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
 
