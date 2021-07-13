2a2b9328456317a7d15653bf87cb66876900eea3
Dawid Gacek <d.gacek@wasko.pl>
2021-07-13 13:59:39 +0200

    removed an unused constructor

--- src/main/java/com/example/demo/dto/Role.java
+++ src/main/java/com/example/demo/dto/Role.java
@@ -1,6 +1,5 @@
 package com.example.demo.dto;
 
-import com.example.demo.entity.RoleEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +14,6 @@ import java.util.Objects;
 public class Role {
     private Long id;
     private String name;
-
-    public Role(RoleEntity roleEntity) {
-        this.id = roleEntity.getId();
-        this.name = roleEntity.getName();
-    }
 
     @Override
     public boolean equals(Object o) {
